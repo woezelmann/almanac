4c17db21813bb792c0240a0b0b85453d5eaa188e
cwozniak <christoph.kamil.wozniak@zalando.de>
2023-06-01 16:17:25 +0200

    refactor dependencies, add failsafe plugin

--- spring-aop/src/test/java/de/woezelmann/ReverseIT.java
+++ spring-aop/src/test/java/de/woezelmann/ReverseIT.java
@@ -1,19 +1,13 @@
 package de.woezelmann;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
